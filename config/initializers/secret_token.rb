c176b9d66e63c4935481d1cb881eb58b61ecfc79
Kevin Musiorski <musiorski@gmail.com>
2012-09-21 07:59:26 -0500

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OtaboAutoTagger::Application.config.secret_token = 'fc0f9f3bcb8bb7308b9cb1787534c0471c96d2cbbc1892595ee7aa46dc1002747bdde612392224f8ad17b87c0e20c0d4fc46293e0d8dd23f803e57f139509af8'
