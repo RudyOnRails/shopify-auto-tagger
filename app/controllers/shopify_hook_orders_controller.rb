4537232f3005589f6f17cd30a45c0a438a21db2f
Kevin Musiorski <musiorski@gmail.com>
2012-09-22 13:27:04 -0500

    added config.shopify stuff to app.rb so :verify_webhook functions hopefully, changed NameSpace from SyncApp to my app's name. Didn't understand it at first. Just pasted and prayed.

--- app/controllers/shopify_hook_orders_controller.rb
+++ app/controllers/shopify_hook_orders_controller.rb
@@ -12,7 +12,7 @@ class ShopifyHookOrdersController < ApplicationController
     data = request.body.read.to_s
     hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
     digest  = OpenSSL::Digest::Digest.new('sha256')
-    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, SyncApp::Application.config.shopify.secret, data)).strip
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, OtaboAutoTagger::Application.config.shopify.secret, data)).strip
     unless calculated_hmac == hmac_header
       head :unauthorized
     end
